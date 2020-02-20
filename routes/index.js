19627d7aa3308c1d23cc305dfad65526d0e8fd7e
axpecial <and043@ucsd.edu>
2020-02-19 17:31:52 -0800

    Set up an alternate Like button and logged click events to prepare for an A/B test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(request, response){
+// Boolean to toggle experimental home page for A/B experiment
+projects["viewAlt"] = false;
+
+exports.view = function(request, response) {
+    projects["viewAlt"] = false;
   	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response) {
+    projects["viewAlt"] = true;
+    response.render('index', projects);
+}
